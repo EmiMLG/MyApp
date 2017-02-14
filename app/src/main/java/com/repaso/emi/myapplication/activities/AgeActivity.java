159d6cbb8e6607c898f5cbd83d9ffa5457d5a095
Emi <emimlg1@gmail.com>
2017-02-14 18:52:01 +0100

    added genderActivity

--- app/src/main/java/com/repaso/emi/myapplication/activities/AgeActivity.java
+++ app/src/main/java/com/repaso/emi/myapplication/activities/AgeActivity.java
@@ -1,13 +1,15 @@
-package com.repaso.emi.myapplication;
+package com.repaso.emi.myapplication.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.repaso.emi.myapplication.R;
+
+public class AgeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_age);
     }
 }
---
+++ app/src/main/java/com/repaso/emi/myapplication/activities/GenderActivity.java
@@ -0,0 +1,41 @@
+package com.repaso.emi.myapplication.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.repaso.emi.myapplication.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class GenderActivity extends AppCompatActivity {
+
+    @BindView(R.id.activity_main___select_gender)TextView selectGender;
+    @BindView(R.id.activity_main___button_man)ImageView buttonMan;
+    @BindView(R.id.activity_main___button_woman)ImageView buttonWoman;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+
+        buttonMan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        buttonWoman.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+}
